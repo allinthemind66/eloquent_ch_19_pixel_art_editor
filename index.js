d8882fd61c586062e1350bda2d5a073632c4bcb2
allinthemind66 <r.nilon92@gmail.com>
2018-08-29 15:54:44 -0400

    create drawPixel function

--- index.js
+++ index.js
@@ -162,3 +162,12 @@ class ColorSelect {
   }
   syncState(state) { this.input.value = state.color; }
 }
+
+function draw(pos, state, dispatch){
+  function drawPixel({x, y}, state){
+    let drawn = {x, y, color: state.color}
+    dispatch({picture: state.picture.draw([drawn])})
+  }
+  drawPixel(pos, state);
+  return drawPixel
+}
