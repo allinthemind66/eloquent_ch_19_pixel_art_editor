dc15a77f5090dc7940fd88140a5f52a2695ec154
allinthemind66 <r.nilon92@gmail.com>
2018-08-29 19:56:18 -0400

    add save button

--- index.js
+++ index.js
@@ -233,3 +233,24 @@ let state = {
 }
 });
 document.querySelector("div").appendChild(app.dom);
+
+class SaveButton {
+  constructor(state){
+    this.picture = state.picture;
+    this.dom = elt("button", {
+      onclick: () => this.save();
+    }, "💾 Save")
+  }
+  save() {
+    let canvas = elt("canvas");
+    drawPicture(this.picture, canvas, 1);
+    let link = elt("a", {
+      href: canvas.toDataURL(),
+      download: "pixelart.png"
+    });
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  }
+  syncState(state) { this.picture = state.picture; }
+}
