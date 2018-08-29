bf5929ca96da4e0e5d02118413425c726f62fb55
allinthemind66 <r.nilon92@gmail.com>
2018-08-29 13:45:05 -0400

    create PixelEditor component

--- index.js
+++ index.js
@@ -111,3 +111,30 @@ PictureCanvas.prototype.touch = function(startEvent, onDown) {
   this.dom.addEventListener("touchmove", move);
   this.dom.addEventListener("touchend", end);
 };
+
+class PixelEditor {
+  constructor(state, config) {
+    let {tools, controls, dispatch} = config;
+    this.state = state;
+    //set the canvas up in Pixel Editor
+    this.canvas = new PictureCanvas(state.picture, pos => {
+      let tool = tools[this.state.tool];
+      let onMove = tool(pos, this.state, dispatch);
+      if (onMove) return pos => onMove(pos, this.state);
+    });
+    //set controls for editor
+    this.controls = controls.map(
+      Control => new Control(state, config)
+    )
+    //set dom for editor
+    //the spaces are so the controls aren't so pressed together
+    this.dom = elt("div", {}, this.canvas.dom, elt("br"), ...this.controls.reduce(
+      (a, c) => a.concat(" ", c.dom), []
+    ));
+    }
+    syncState(state) {
+      this.state = state;
+      this.canvas.syncState(state.picture);
+      for(let ctrl of this.controls) ctrl.syncState(state);
+  }
+}
